2156f117ef56c94d92a5ad6f0eb8ffdb03f967f7
Clauveira <claue.silveira@gmail.com>
2022-05-01 10:35:55 -0300

    Skybox pilha prefab physic material

--- Assets/Code/Jogador.cs
+++ Assets/Code/Jogador.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 
 public class Jogador : MonoBehaviour
 {
-    private Rigidbody player_Rigidbody;
-    private bool eh_mobile = false;
+    private Rigidbody Faca_Rigidbody;
+    public MeshCollider Faca_Collider;
+    private bool Eh_Mobile = false;
     void Start()
     {
-        player_Rigidbody = GetComponent<Rigidbody>();
-        eh_mobile = Application.platform == RuntimePlatform.Android;
+        Faca_Rigidbody = GetComponent<Rigidbody>();
+        Eh_Mobile = Application.platform == RuntimePlatform.Android;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space") || (eh_mobile && Input.GetTouch(0).phase == TouchPhase.Began))
+        if (Input.GetKeyDown("space") || (Eh_Mobile && Input.GetTouch(0).phase == TouchPhase.Began))
         {
-            player_Rigidbody.AddForce(new Vector3(70f, 300f, 0f));
-            player_Rigidbody.AddTorque(new Vector3(0f, 0f, -30f));
+            Faca_Rigidbody.AddForce(new Vector3(70f, 300f, 0f));
+            Faca_Rigidbody.AddTorque(new Vector3(0f, 0f, -30f));
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.GetComponent<ObjetoFatiavel>() != null)
+        {
+            other.gameObject.GetComponent<ObjetoFatiavel>().Fatiar();
         }
     }
 }
--- Assets/Code/ObjetoFatiavel.cs
+++ Assets/Code/ObjetoFatiavel.cs
@@ -6,6 +6,8 @@ public class ObjetoFatiavel : MonoBehaviour
 {
     public GameObject Fatia1;
     public GameObject Fatia2;
+    private Rigidbody Fatia1_Rigidbody;
+    private Rigidbody Fatia2_Rigidbody;
     void Start()
     {
 
@@ -14,10 +16,6 @@ public class ObjetoFatiavel : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown("space"))
-        {
-            Fatiar();
-        }
     }
 
     public void Fatiar()
@@ -26,7 +24,17 @@ public class ObjetoFatiavel : MonoBehaviour
         {
             Destroy(GetComponent<Rigidbody>());
             Fatia1.AddComponent<Rigidbody>();
-            Fatia2.AddComponent<Rigidbody>();
+            Fatia2_Rigidbody = Fatia2.AddComponent<Rigidbody>();
+
+            Fatia1_Rigidbody = Fatia1.GetComponent<Rigidbody>();
+            Fatia2_Rigidbody = Fatia2.GetComponent<Rigidbody>();
+
+            Fatia1_Rigidbody.mass = 0.1f;
+            Fatia2_Rigidbody.mass = 0.1f;
+
+            Fatia1_Rigidbody.AddForce(new Vector3(0f, 5f, 10f));
+            Fatia2_Rigidbody.AddForce(new Vector3(0f, 5f, -10f));
+
         }
     }
 }
